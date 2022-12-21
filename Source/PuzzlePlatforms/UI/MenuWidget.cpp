d945e06bf86deb298b8b774d6a15f1002f2c4686
AlexanderFengFeng <39276953+AlexanderFengFeng@users.noreply.github.com>
2022-12-20 18:11:25 -0800

    Create UMenuWidget class for setup and teardown.
    Derived from UMainMenu and will be used for any menu widgets that need to be rendered.

---
+++ Source/PuzzlePlatforms/UI/MenuWidget.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MenuWidget.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+
+void UMenuWidget::Setup()
+{
+    this->AddToViewport();
+    if (PlayerController == nullptr) return;
+    FInputModeUIOnly InputModeData;
+    InputModeData.SetWidgetToFocus(this->TakeWidget());
+    InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+    PlayerController->SetInputMode(InputModeData);
+    PlayerController->bShowMouseCursor = true;
+}
+
+void UMenuWidget::OnLevelRemovedFromWorld(ULevel* InLevel, UWorld* InWorld)
+{
+    Super::OnLevelRemovedFromWorld(InLevel, InWorld);
+    this->RemoveFromViewport();
+    if (PlayerController == nullptr) return;
+    FInputModeGameOnly InputModeData;
+    PlayerController->SetInputMode(InputModeData);
+    PlayerController->bShowMouseCursor = false;
+}
+
+void UMenuWidget::SetMenuInterface(IMenuInterface* InMenuInterface)
+{
+    MenuInterface = InMenuInterface;
+}
+
+void UMenuWidget::QuitGame()
+{
+    UKismetSystemLibrary::QuitGame(this, PlayerController, EQuitPreference::Quit, false);
+}
